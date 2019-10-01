3954e1db965033d6fb0eec2431dfa7b19ea57ac7
Hải Sơn Nguyễn <nguyensonhai1009@gmail.com>
2019-10-01 23:08:14 +0700

    backend_started

--- ezapp/urls.py
+++ ezapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('main/', views.main),
-    path('loading/', views.loading),
+    path('main/', views.main, name='main'),
+    path('loading/', views.loading, name='loading'),
+    path('signup/', views.signup, name='signup'),
 ]
