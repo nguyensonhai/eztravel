172b927fe6deef2ff8c1758043faac8b4fa81f71
nguyensonhai <nguyensonhai1009@gmail.com>
2019-11-30 22:18:03 +0700

    updatE

--- ezapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class signUp(forms.Form):
-    username= forms.CharField(label='Username', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Type your username'}))
-    email = forms.CharField(label='Email', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Type your email'}))
-    password = forms.CharField(label='Password', max_length=20, widget=forms.PasswordInput(attrs={'placeholder': 'Type your password'}))
-    repassword = forms.CharField(label='Re-type Password', max_length=20, widget=forms.PasswordInput(attrs={'placeholder': 'Re-type your password'}))
